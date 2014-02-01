0faae2407c942e3f519c02cc77cbc37f3b53da4a
James Hiscock <james@small.mu>
2014-02-01 15:41:42 +1100

    Set attribute.

--- attributes/set.js
+++ attributes/set.js
@@ -1,6 +1,10 @@
 var remove = require('./remove')
+var properties = require('./properties')
 
 module.exports = function setAttribute(el, prop, val) {
     if (val === null) return remove(el, prop)
+    if (properties.test(prop)) {
+        return el.setAttribute(prop, prop)
+    }
     return el.setAttribute(prop, val + '')
 }
