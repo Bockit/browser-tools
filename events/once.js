c9c1c3f0d13f2bf8dc7daef0839a4e784cb9c9a8
James Hiscock <james@small.mu>
2014-02-09 11:43:19 +1100

    Made once use off to remove.

--- events/once.js
+++ events/once.js
@@ -1,8 +1,9 @@
 var on = require('./on')
+var off = require('./off')
 
 module.exports = function once(el, event, callback) {
     return on(el, event, function() {
-        el.removeEventListener(event, callback)
+        off(el, event, callback)
         callback.apply(null, arguments)
     })
 }
