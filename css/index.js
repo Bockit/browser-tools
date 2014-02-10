d2d975fbadc3a495a2af8f65bd9499ec1116478c
Calvein <calvein@gmail.com>
2014-02-10 11:41:55 +1100

    Make css automatically choose between getter and setter

--- css/index.js
+++ css/index.js
@@ -1,4 +1,6 @@
-module.exports = {
-    get: require('./get')
-  , set: require('./set')
+var get = require('./get')
+var set = require('./set')
+
+module.exports = function(el, prop, val) {
+    return typeof val !== 'undefined' ? set(el, prop, val) : get(el, prop)
 }
