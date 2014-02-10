dfaa5207256e6898dc3af00a74c56d61b1e5d8c1
Calvein <calvein@gmail.com>
2014-02-10 11:42:30 +1100

    Make css.set taking care of adding 'px' for some values

--- css/set.js
+++ css/set.js
@@ -1,5 +1,19 @@
 var prefixed = require('./prefix-map')()
 
+// Properties that are number but can't have a px value
+var cssNumbers = {
+    columnCount: true
+  , fillOpacity: true
+  , fontWeight: true
+  , lineHeight: true
+  , opacity: true
+  , order: true
+  , orphans: true
+  , widows: true
+  , zIndex: true
+  , zoom: true
+}
+
 module.exports = function setCss(el, prop, val) {
     if (typeof prop === 'object') {
         for (key in prop) {
@@ -7,6 +21,8 @@ module.exports = function setCss(el, prop, val) {
         }
     }
     else {
+        if (typeof val === 'number' && !cssNumbers[prop])
+            val += 'px'
         el.style[prefixed[prop]] = val
     }
     return el
