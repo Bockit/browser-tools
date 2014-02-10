240ef03459ae1e1017e2441f2752dbbb0d57d650
Calvein <calvein@gmail.com>
2014-02-10 15:18:17 +1100

    Add object for attributes

--- attributes/set.js
+++ attributes/set.js
@@ -2,12 +2,20 @@ var remove = require('./remove')
 var properties = require('./properties')
 
 module.exports = function setAttribute(el, prop, val) {
-    if (val === null) return remove(el, prop)
-    if (properties.test(prop)) {
-        el.setAttribute(prop, prop)
-    }
-    else {
-        el.setAttribute(prop, val + '')
+    if (typeof prop === 'object') {
+        for (var key in prop) {
+            setAttribute(el, key, prop[key])
+        }
+    } else {
+        if (val === null) return remove(el, prop)
+        // @TODO if val is function
+        // args are i and the attr
+        if (properties.test(prop)) {
+            el.setAttribute(prop, prop)
+        }
+        else {
+            el.setAttribute(prop, '' + val)
+        }
     }
     return el
 }
