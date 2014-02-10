3724dd2507b05e6f46e19c4a0442387b92f27a56
James Hiscock <james@small.mu>
2014-02-11 08:08:42 +1100

    attr/index and css/index just export the function with props attached.

--- attributes/index.js
+++ attributes/index.js
@@ -3,14 +3,14 @@ var set        = require('./set')
 var remove     = require('./remove')
 var properties = require('./properties')
 
-module.exports = attr = {}
+module.exports = attr
 
 attr.getAttr    = get
 attr.setAttr    = set
 attr.removeAttr = remove
 attr.properties = properties
 
-attr.attr = function attr(el, prop, val) {
+function attr(el, prop, val) {
     return typeof val !== 'undefined' || typeof prop === 'object' ?
         set(el, prop, val) :
         get(el, prop)
--- css/index.js
+++ css/index.js
@@ -1,12 +1,12 @@
 var get = require('./get')
 var set = require('./set')
 
-module.exports = css = {}
+module.exports = css
 
 css.getCss = get
 css.setCss = set
 
-css.css = function css(el, prop, val) {
+function css(el, prop, val) {
     return typeof val !== 'undefined' || typeof prop === 'object' ?
         set(el, prop, val) :
         get(el, prop)
