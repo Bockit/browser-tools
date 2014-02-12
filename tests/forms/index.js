0c2eaa32385a1fba50f544cc63808f6698e443d3
James Hiscock <james@small.mu>
2014-02-12 14:12:35 +1100

    First pass at forms. Specifically, values.

---
+++ tests/forms/index.js
@@ -0,0 +1,64 @@
+var test = require('tape')
+  , get = require('../../forms/value/get')
+  , set = require('../../forms/value/set')
+  , els = {
+        text: document.getElementById('input-text')
+      , hidden: document.getElementById('input-hidden')
+      , password: document.getElementById('input-password')
+      , textarea: document.getElementById('textarea')
+    }
+  , valuedEls = {
+        text: document.getElementById('input-text-valued')
+      , hidden: document.getElementById('input-hidden-valued')
+      , password: document.getElementById('input-password-valued')
+      , textarea: document.getElementById('textarea-valued')
+  }
+
+  console.log(valuedEls)
+
+test('Initially with no values', function(t) {
+    for(var k in els) {
+        var el = els[k]
+        t.equal(get(el), '', k + ' is an empty string')
+    }
+
+    t.end()
+})
+
+test('Initially with some values', function(t) {
+    for(var k in valuedEls) {
+        var el = valuedEls[k]
+        t.equal(get(el), 'foo', k + ' is foo')
+    }
+
+    t.end()
+})
+
+test('Programmatically set and get some values', function(t) {
+    for(var k in els) {
+        var el = els[k]
+        t.equal(get(set(el, 'foo')), 'foo', k + ' is foo')
+    }
+
+    t.end()
+})
+
+test('Programatically remove initial values', function(t) {
+    for(var k in valuedEls) {
+        var el = valuedEls[k]
+
+        t.equal(get(set(el, null)), '', k + ' is an empty string')
+    }
+
+    t.end()
+})
+
+test('Programatically remove programatically set values', function(t) {
+    for(var k in els) {
+        var el = els[k]
+
+        t.equal(get(set(set(el, 'foo'), null)), '', k + ' is an empty string')
+    }
+
+    t.end()
+})
