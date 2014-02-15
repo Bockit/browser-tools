16bebb2020e29bc3d5c573ae022d5367fdb3f481
James Hiscock <james@small.mu>
2014-02-15 17:46:52 +1100

    First pass at delegation, works for on but isn't tracked internally and used in off yet.

--- events/on.js
+++ events/on.js
@@ -4,19 +4,25 @@ var normaliseMouse = require('./normalise/mouse')
 var normaliseKey = require('./normalise/key')
 var tracker = require('./tracker')
 var nonce = require('./nonce')
+var matches = require('../query/matches')
+var between = require('../traversal/between')
 
 module.exports = on
 
-function on(el, event, callback) {
+function on(el, event, selector, callback) {
     if (!nonce.has(el)) nonce.set(el, nonce.gen())
+    if (arguments.length === 3) {
+        callback = selector
+        selector = null
+    }
 
-    var wrapped = callback
+    wrapped = selector ? delegate(callback, selector) : callback
 
     if (mouseEvents.test(event)) {
-         wrapped = normaliseMouse(callback)
+         wrapped = normaliseMouse(wrapped)
     }
     else if (keyEvents.test(event)) {
-        wrapped = normaliseKey(callback)
+        wrapped = normaliseKey(wrapped)
     }
 
     tracker.add(nonce.get(el), event, wrapped, callback)
@@ -25,4 +31,21 @@ function on(el, event, callback) {
     el.addEventListener(type, wrapped)
 
     return el
+}
+
+function delegate(callback, selector) {
+    return function(ev) {
+        if (matches(ev.target, selector)) {
+            ev.delegateTarget = ev.target
+            callback.call(null, ev)
+        }
+
+        var els = between(ev.target, ev.currentTarget)
+        for(var i = 0; i < els.length; i++) {
+            if (matches(els[i], selector)) {
+                ev.delegateTarget = els[i]
+                callback.call(null, ev)
+            }
+        }
+    }
 }
